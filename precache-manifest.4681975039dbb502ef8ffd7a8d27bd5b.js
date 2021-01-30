9e11b107688a1a1322762a0e72da6004d9b7c611
sumeetmohite <sumeetmohite9@gmail.com>
2021-01-30 23:30:07 +0530

    Updates

---
+++ precache-manifest.4681975039dbb502ef8ffd7a8d27bd5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0d029081485991f37a13a889650a099",
+    "url": "/tailwindfolio/index.html"
+  },
+  {
+    "revision": "1f506b031b58fb6247c8",
+    "url": "/tailwindfolio/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "212808b29303c175b489",
+    "url": "/tailwindfolio/static/css/main.c3771c4c.chunk.css"
+  },
+  {
+    "revision": "1f506b031b58fb6247c8",
+    "url": "/tailwindfolio/static/js/2.c2e7f024.chunk.js"
+  },
+  {
+    "revision": "2f05789a8b6fe15b70e274abbac523b1",
+    "url": "/tailwindfolio/static/js/2.c2e7f024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212808b29303c175b489",
+    "url": "/tailwindfolio/static/js/main.0c9ab0ec.chunk.js"
+  },
+  {
+    "revision": "42663413862c0b7df49a",
+    "url": "/tailwindfolio/static/js/runtime-main.49547667.js"
+  }
+]);
